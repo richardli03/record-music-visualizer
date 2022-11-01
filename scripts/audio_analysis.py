7b960804e6075bb5d6f62c3c15d1817ba7b835a7
richardli03 <rli@olin.edu>
2022-11-01 10:54:19 -0400

    fixed frequency spread

--- scripts/audio_analysis.py
+++ scripts/audio_analysis.py
@@ -1,10 +1,15 @@
-import librosa as l
-import librosa.display as disp
+import os
+import sys
+repository_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+sys.path.append(repository_root)
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.fft as f
 import pandas as pd
-
+import librosa as l
+import librosa.display as disp
 
 INPUT_FILE = "../assets/bass.wav"
 # INPUT_FILE = "assets/365.wav"
@@ -13,10 +18,10 @@ FREQ = True
  
 song, sr = l.load(INPUT_FILE, mono = True)
 
-def make_freq_plot(x, Fs, plot):
+def make_freq_spread(x, Fs, plot):
     """
     Make a frequency domain plot with labels
-
+    
     Args:
         x (array): song data
         Fs (int): sampling rate
@@ -35,8 +40,17 @@ def make_freq_plot(x, Fs, plot):
     song_data = (1/len(x))*f.fftshift(abs(f.fft(x)))
     return song_data, freq_space
 
-def make_time_plot(x, Fs):
-    plt.plot(song)
+def plot_song(x):
+    plt.plot(x)
+    plt.show()
+    
+def data_splitter(freq_data):
+  """
+  Split and process all of the frequency data into 3 different data sets: bass, mid, and treble
+
+  Args:
+      freq_data (pandas Dataframe): a dataframe with all of the frequency data over the course of a song
+  """
 
 def main():
   print(len(song))
@@ -52,9 +66,13 @@ def main():
 
     for i, sample in enumerate(samples[:-1]):
       print(song[samples[i]:samples[i+1]])
-      freq_data, freq_space, sub1 = make_freq_plot(song[samples[i]:samples[i+1]],sr)
+      freq_data, freq_space = make_freq_spread(song[samples[i]:samples[i+1]],sr, True)
       all_freq_data[sample] = pd.Series(l.amplitude_to_db(freq_data))
-    all_freq_data.to_csv("all_freq_data.csv")
+      plt.show()
+      plt.ylabel('Magnitude of signal')
+      plt.xlabel('Frequency (Hz)')
+    
+    # all_freq_data.to_csv("all_freq_data.csv")
 
   if DISPLAY: 
     plt.show()

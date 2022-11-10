8d7cf4edef89ff8aa1d53274c07ba3e64c748566
thegalaxykat <katcanavan@gmail.com>
2022-11-10 17:20:51 -0500

    Serial

--- firmware/arduino_firmware/arduino_firmware.ino
+++ firmware/arduino_firmware/arduino_firmware.ino
@@ -19,3 +19,45 @@ DC motor
 ENB -> pin 2
 IN 4 -> pin 3
 */
+
+#include <Arduino.h>
+#include <L298N.h>
+#include "BasicStepperDriver.h"
+
+// stepper motor steps per revolution (200 steps = 1.8 degrees/step)
+#define MOTOR_STEPS 200
+#define RPM 120
+// microstepping not set externally (1 = full step)
+#define MICROSTEPS 1
+
+// define wires
+#define DIR1 8
+#define STEP1 9
+
+
+// global variables
+String command;
+
+void setup() {
+  // initialize serial communication
+  Serial.begin(115200);
+
+}
+
+
+void loop() {
+readSerialBuffer();
+
+}
+
+
+// **funtions defined below this point**
+
+// read serial and update command string
+void readSerialBuffer() {
+  while (Serial.available() == 0) {
+    // wait for data available in serial receive buffer
+  }
+  command = Serial.readStringUntil('\n');  // read until timeout
+  command.trim();                          // remove white space or \n
+}

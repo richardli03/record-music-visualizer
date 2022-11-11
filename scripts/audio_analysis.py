42d4bf919f0a4d3b3c470a785d8f6e0bed99c603
richardli03 <rli@olin.edu>
2022-11-11 17:09:04 -0500

    added ceiling to frequency

--- scripts/audio_analysis.py
+++ scripts/audio_analysis.py
@@ -2,7 +2,7 @@ import library as lib
 
 def main():
   
-  INPUT_FILE = "../assets/mids.wav" # audio input
+  INPUT_FILE = "../assets/365.wav" # audio input
   bot, mot, tot = lib.plot_volumes(INPUT_FILE)
   lib.draw_record_visual(bot, mot, tot)
   

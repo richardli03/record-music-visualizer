e77b49618f9b8b8e73076c4418b41636b163faf6
Richard Li <richardli485@gmail.com>
2022-11-11 17:13:44 -0500

    set input file to mids.wav

--- scripts/audio_analysis.py
+++ scripts/audio_analysis.py
@@ -2,8 +2,7 @@ import library as lib
 
 def main():
   
-  INPUT_FILE = "../assets/mids
-  .wav" # audio input
+  INPUT_FILE = "../assets/mids.wav" # audio input
   bot, mot, tot = lib.plot_volumes(INPUT_FILE)
   lib.draw_record_visual(bot, mot, tot)
   

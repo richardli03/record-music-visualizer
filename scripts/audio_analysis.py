481090af48a2f22ab15a0f5933a68f427b60018b
Isha-Goyal <goyalisha03@gmail.com>
2022-11-11 11:29:42 -0500

    Final streamline commit.
    Everything currently works to make the baseline visualization.
    Will be merged with main.

--- scripts/audio_analysis.py
+++ scripts/audio_analysis.py
@@ -3,8 +3,8 @@ import library as lib
 def main():
   
   INPUT_FILE = "../assets/mids.wav" # audio input
-  lib.plot_volumes(INPUT_FILE)
-  lib.create_csv()
+  bot, mot, tot = lib.plot_volumes(INPUT_FILE)
+  lib.draw_record_visual(bot, mot, tot)
   
 if __name__ == "__main__":
     main()

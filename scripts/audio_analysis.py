abca2a6dc2f2fa5216d1b43811a9ac50c38fcb0f
Richard Li <richardli485@gmail.com>
2022-11-11 17:12:57 -0500

    fixing merge conflict
    change 365 to mids

--- scripts/audio_analysis.py
+++ scripts/audio_analysis.py
@@ -2,7 +2,8 @@ import library as lib
 
 def main():
   
-  INPUT_FILE = "../assets/365.wav" # audio input
+  INPUT_FILE = "../assets/mids
+  .wav" # audio input
   bot, mot, tot = lib.plot_volumes(INPUT_FILE)
   lib.draw_record_visual(bot, mot, tot)
   

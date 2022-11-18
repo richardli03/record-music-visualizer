edf5d4a74af970775712567e09ef053d80d5099c
richardli03 <rli@olin.edu>
2022-11-18 11:24:54 -0500

    integrated stepper objects, waiting for calibration data

--- drivers/parser.py
+++ drivers/parser.py
@@ -7,14 +7,27 @@ import time
 import pandas as pd
 
 class Stepper:
-
     def __init__(self, name):
         self.name = name
-        
         pass
+    
+    def move(self, value):
+        if self.name == "bass":
+            # move calibration
+            pass
+            
+        if self.name == "mid":
+            # move calibration
+            pass
+            
+        if self.name == "treb":
+            # move calibration
+            pass
     
     def __repr__(self) -> str:
         return f"Stepper motor {self.name}"
+
+
 
 def compute_DC_speed(song_length) -> float:
     """
@@ -28,30 +41,40 @@ def compute_DC_speed(song_length) -> float:
 # First thing to do is rotate the thing at the correct speed:
 # send_serial.dc_speed(200) #compute_DC_speed
 
-# bass = Stepper("bass")
-# mid = Stepper("mid")
-# treble = Stepper("treble")
+def main():
+    bass = Stepper("bass")
+    mid = Stepper("mid")
+    treble = Stepper("treb")
 
-song = True
+    song_data = pd.read_csv("datasets/pos_data.csv").transpose()
+    print(song_data)
+    
+    #send_serial.dc_speed(compute_DC_speed(song_length))
 
-song_data = pd.read_csv("datasets/test.csv").transpose()
-print(song_data)
-if not song_data.empty:
-    next_sample = song_data[0]
 
-num_samples = list(song_data.columns)
+    if not song_data.empty:
+        next_sample = song_data[0]
 
-for sample in num_samples:
-    # Split the new row of values into their components
-    next_sample = song_data[sample]
-    vals = next_sample.to_list()
+    num_samples = list(song_data.columns)
 
-    # TODO: We do have to connect time with this angle
-    theta = vals[0]
+    for sample in num_samples:
+        # Split the new row of values into their components
+        
+        next_sample = song_data[sample]
+        vals = next_sample.to_list()
 
-    # Get bass, mid, treble frequences
-    bass = vals[1]
-    mid = vals[2]
-    treble = vals[3]
-    # Compute the stepper motor movement to correspond with an int
-       
+        # TODO: We do have to connect time with this angle
+        theta = vals[0]
+
+        # Get bass, mid, treble frequences
+        bass.move(vals[1])
+        mid.move(vals[2])
+        treble.move(vals[3])      
+        
+        print(bass, mid, treble)
+        
+        # Compute the stepper motor movement to correspond with an int
+
+
+if __name__ == "__main__":
+    main()

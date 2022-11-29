d9c00fe0279063c944fc9a8940d2e7469b3846a3
richardli03 <rli@olin.edu>
2022-11-29 15:29:44 -0500

    added framework parser code with calibration equations

--- drivers/parser.py
+++ drivers/parser.py
@@ -3,29 +3,44 @@ turn the DC motor on and spin the servo for a few seconds
 """
 
 import time
-# import send_serial
+from send_serial import *
 import pandas as pd
 
 class Stepper:
     def __init__(self, name):
-        self.name = name
+        self._name = name
         pass
     
     def move(self, value):
-        if self.name == "bass":
-            # move calibration
-            pass
+        """
+        As a reminder to me, the "value" is going to be a 
+        rigid distance away from its "center line"
+
+        Args:
+            value (_type_): _description_
+        """
+        move_amount = 1139*value + -17.9
+        
+        if self._name == "bass":
             
-        if self.name == "mid":
             # move calibration
-            pass
+            stepper("a", move_amount)
+            return
             
-        if self.name == "treb":
+        if self._name == "mid":
             # move calibration
+            
+            stepper("b", move_amount)
+            return
+            
+        if self._name == "treb":
+            # move calibration
+            
+            stepper("c", move_amount)
             pass
     
     def __repr__(self) -> str:
-        return f"Stepper motor {self.name}"
+        return f"Stepper motor {self._name}"
 
 
 
@@ -34,14 +49,16 @@ def compute_DC_speed(song_length) -> float:
     Given some song length (in seconds), compute
     the speed of the DC motor necessary to make that happen
     """
-
-    #return motor_speed
-    pass
-
-# First thing to do is rotate the thing at the correct speed:
-# send_serial.dc_speed(200) #compute_DC_speed
-
+    # line of best fit 
+    motor_speed = 182 + -1.19*song_length + 0.0026*(song_length^2)
+    return motor_speed
+    
+    
 def main():
+    # First thing to do is rotate the thing at the correct speed:
+    dc_speed(200) #compute_DC_speed
+    
+    
     bass = Stepper("bass")
     mid = Stepper("mid")
     treble = Stepper("treb")
